a0c525e09bbb69fa9019fc8db3de8a8b747997bb
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-08-24 02:52:18 +1200

    added something

--- paloozay/components/Nav.jsx
+++ paloozay/components/Nav.jsx
@@ -12,11 +12,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link
-        href='/'
-        className='flex  gap-2 flex-center'
-        style={{ width: '30px' }}
-      >
+      <Link href='/' className='flex  gap-2 flex-center'>
         <Logo />
       </Link>
     </nav>
